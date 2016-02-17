922c89741455ae01d091061d94df2d1b62c313eb
Crazyglue <edbz2k2@gmail.com>
2016-02-16 21:13:23 -0600

    next one

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.4.0'
 
 set :application, 'schapendoes'
 set :repo_url, 'git@github.com:Crazyglue/schapendoes.git' # Edit this to match your repository
-set :branch, :master
+set :branch, :deployment
 set :deploy_to, '/home/deploy/schapendoes'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
