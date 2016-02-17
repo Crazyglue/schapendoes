765882c2d48a4bee55e641e5bcae4a0813cef322
Crazyglue <edbz2k2@gmail.com>
2016-02-17 15:37:54 -0600

    Add form functionality

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,19 @@
 class WelcomeController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
+    @message = Message.new
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.create(email: params[:email], name: params[:name], content: params[:message])
+    if @message.save
+      flash[:notice] = "Thanks for the message!"
+      redirect_to '/'
+    end
   end
 end
